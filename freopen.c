978a6d3e58104954504d13028409ee7fd09c0f3b
AbhidnyaKatruwar <abhidnyark008@gmail.com>
2016-11-02 19:04:59 +0530

    Delete freopen.c

--- freopen.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include"file.h"
-
-_FILE *f_reopen(char *filename, char *mode, _FILE *stream) { 
-  if (stream -> fd > 0) 
-	f_close(stream);
-  return f_open(filename, mode) ;
-}
-  
